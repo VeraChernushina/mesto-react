6691a2f18e720b32fb10f9783997197ddd973f1f
Vera Chernushina <vera.chernushina@yandex.ru>
2022-01-03 23:05:34 +0300

    feat: get user data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,38 @@
 import React from "react";
-
-import avatarLoading from "../images/avatar-loading.png";
+import api from "../utils/api";
 
 const Main = props => {
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
+
+  React.useEffect(() => {
+    api.getUserInfo()
+    .then((data) => {
+      setUserName(data.name);
+      setUserDescription(data.about);
+      setUserAvatar(data.avatar);
+    })
+    .catch((err) => {
+      console.log(`Ошибка: ${err}`);
+    });
+  })
+
   return (
       <main className="content">
         <section className="profile page__profile">
           <div className="profile__card">
             <img
               className="profile__avatar"
-              src={avatarLoading}
+              src={userAvatar}
               alt="Аватар пользователя"
             />
             <button className="profile__avatar-btn" onClick={props.onEditAvatar}></button>
             <div className="profile__info">
-              <h1 className="profile__title"></h1>
+              <h1 className="profile__title">{userName}</h1>
               <button type="button" className="profile__edit-button"
               onClick={props.onEditProfile}></button>
-              <p className="profile__description"></p>
+              <p className="profile__description">{userDescription}</p>
             </div>
           </div>
 
