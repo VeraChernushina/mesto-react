9c67321d38a4595bdbbd283353631481ee11130e
Vera Chernushina <vera.chernushina@yandex.ru>
2022-01-17 20:03:44 +0300

    feat: add active like-button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 const Card = ({ card, onCardClick }) => {
+  const currentUser = useContext(CurrentUserContext);
+
   const handleClick = () => {
     onCardClick(card);
   };
+
+// Определяем, являемся ли мы владельцем текущей карточки
+const isOwn = card.owner._id === currentUser._id;
+
+// Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+const isLiked = card.likes.some(i => i._id === currentUser._id);
+// Создаём переменную, которую после зададим в `className` для кнопки лайка
+const cardLikeButtonClassName = (
+  `element__like-btn ${isLiked ? 'element__like-btn_active' : ''}`
+); 
 
   return (
     <div className="element">
@@ -16,10 +29,10 @@ const Card = ({ card, onCardClick }) => {
       <div className="element__bottom-block">
         <h3 className="element__title">{card.name}</h3>
         <div className="element__likes">
-          <button type="button" className="element__like-btn"></button>
+          <button type="button" className={cardLikeButtonClassName}></button>
           <span className="element__likes-number">{card.likes.length}</span>
         </div>
-        <button type="button" className="element__delete-btn"></button>
+        {isOwn && <button type="button" className='element__delete-btn' />}
       </div>
     </div>
   );
